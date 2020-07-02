fbbe158040a982fa0b23ad8db955f33d6ce9aa14
Jin Shi <jinshi@google.com>
2020-07-02 21:56:44 +0800

    [keras] Format the code and add accuracy.

--- keras/keras_rnn.py
+++ keras/keras_rnn.py
@@ -2,32 +2,33 @@ import tensorflow as  tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-mnist = tf.keras.datasets.mnist
+def build_model():
+  model = Sequential()
+  model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu',
+                 return_sequences=True))
+  model.add(Dropout(0.2))
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+  model.add(LSTM(128, activation='relu'))
+  model.add(Dropout(0.2))
 
-print(x_train[0].shape)
+  model.add(Dense(32, activation='relu'))
+  model.add(Dropout(0.2))
 
-# bulid model
-model = Sequential()
-model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu',
-return_sequences=True))
-model.add(Dropout(0.2))
+  model.add(Dense(10, activation='softmax'))
 
-model.add(LSTM(128, activation='relu'))
-model.add(Dropout(0.2))
+  return model
 
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.2))
 
-model.add(Dense(10, activation='softmax'))
+if __name__ == '__main__':
+  mnist = tf.keras.datasets.mnist
+  (x_train, y_train), (x_test, y_test) = mnist.load_data()
+  x_train = x_train / 255.0
+  x_test = x_test / 255.0
 
-opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer=opt,
-              metrics=['accuracy'])
+  model = build_model()
+  opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
+  model.compile(loss='sparse_categorical_crossentropy', optimizer=opt,
+                metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=3, validation_data=(x_test, y_test))
-
+  model.fit(x_train, y_train, epochs=3, validation_data=(x_test, y_test))
